e25fe141d103a6d5eb6d50b217dfe4898094fbf8
Macxxon <maicolalarcon93@gmail.com>
2024-03-11 22:38:07 -0500

    phone authentication

--- app/src/main/java/com/make/develop/studio/AuthActivity.kt
+++ app/src/main/java/com/make/develop/studio/AuthActivity.kt
@@ -1,6 +1,8 @@
 package com.make.develop.studio
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.PhoneAuthCredential
@@ -14,58 +16,76 @@ class AuthActivity:AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: ActivityAuthBinding
+    private var verificationId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.generateBtn.setOnClickListener {
+            val phoneNumber = binding.phoneNumberEdt.text.toString()
+            startPhoneNumberVerification(phoneNumber)
+        }
+
+        binding.otpVerifyBtn.setOnClickListener {
+            val code = binding.otpNumberEdt.text.toString()
+            if(verificationId.isNullOrEmpty()){
+                Toast.makeText(this, "No se ha enviado el código de verificación", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            verifyPhoneNumberWithCode(verificationId!!, code)
+        }
     }
 
     private fun startPhoneNumberVerification(phoneNumber: String) {
         val options = PhoneAuthOptions.newBuilder(firebaseAuth)
-            .setPhoneNumber(phoneNumber)       // Número de teléfono a verificar
-            .setTimeout(60L, TimeUnit.SECONDS)  // Duración del tiempo de espera para ingresar el código
+            .setPhoneNumber("+57$phoneNumber")
+            .setTimeout(60L, TimeUnit.SECONDS)
             .setActivity(this)
             .setCallbacks(callbacks)
             .build()
 
         PhoneAuthProvider.verifyPhoneNumber(options)
+
+        binding.layoutVerify.visibility = View.GONE
+        binding.generateBtn.visibility = View.GONE
+        binding.layoutOtp.visibility = View.VISIBLE
+        binding.otpVerifyBtn.visibility = View.VISIBLE
+
     }
 
-    // Método para verificar el código ingresado por el usuario
     private fun verifyPhoneNumberWithCode(verificationId: String, code: String) {
         val credential = PhoneAuthProvider.getCredential(verificationId, code)
         signInWithPhoneAuthCredential(credential)
     }
 
-    // Método para autenticar al usuario con las credenciales del número de teléfono
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
         firebaseAuth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Autenticación exitosa, puedes redirigir al usuario o realizar otras acciones
-                    // Por ejemplo, puedes iniciar una nueva actividad.
+                    Toast.makeText(this@AuthActivity, "Logeado mijo", Toast.LENGTH_SHORT).show()
                 } else {
-                    // La autenticación falló, maneja la situación según tus necesidades
+                    Toast.makeText(this@AuthActivity, "Ha ocurrido un error", Toast.LENGTH_SHORT).show()
                 }
             }
     }
 
-    // Callbacks para gestionar eventos de autenticación con el número de teléfono
     private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
         override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-            // Se llama cuando la verificación se completa automáticamente
             signInWithPhoneAuthCredential(credential)
         }
 
         override fun onVerificationFailed(e: FirebaseException) {
-            // Se llama cuando la verificación falla, maneja la situación según tus necesidades
+            Toast.makeText(this@AuthActivity, "Error al enviar el código de verificación", Toast.LENGTH_SHORT).show()
         }
 
         override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
-            // Se llama cuando el código de verificación es enviado exitosamente al número de teléfono
-            // Puedes almacenar el "verificationId" para usarlo más tarde
+            super.onCodeSent(verificationId, token)
+            this@AuthActivity.verificationId = verificationId
+            Toast.makeText(this@AuthActivity, "Código de verificación enviado correctamente", Toast.LENGTH_SHORT).show()
         }
     }
 
