e6e3ffa7befad3923c41eed1736e02db2e578441
Macxxon <maicolalarcon93@gmail.com>
2024-03-11 21:29:27 -0500

    update version

--- app/src/main/java/com/make/develop/studio/MainActivity.kt
+++ app/src/main/java/com/make/develop/studio/MainActivity.kt
@@ -2,10 +2,20 @@ package com.make.develop.studio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.make.develop.studio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        firebaseAuth = Firebase.auth
     }
 }
