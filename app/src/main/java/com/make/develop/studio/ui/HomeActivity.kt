fb62433fecad0695535526bb4432dcc9bc14ac52
Macxxon <maicolalarcon93@gmail.com>
2024-03-14 23:22:01 -0500

    notification permission

--- app/src/main/java/com/make/develop/studio/ui/HomeActivity.kt
+++ app/src/main/java/com/make/develop/studio/ui/HomeActivity.kt
@@ -1,8 +1,17 @@
 package com.make.develop.studio.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.snackbar.Snackbar
 import com.make.develop.studio.R
 import com.make.develop.studio.databinding.ActivityHomeBinding
 
@@ -25,5 +34,47 @@ class HomeActivity :AppCompatActivity(){
                     //TODO: Add the id of the fragment that you want to hide the bottom navigation view
                 }
             }
+
+        askNotificationPermission()
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                //later
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Permisos ortorgados", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+
+            Snackbar.make(
+                binding.root,
+                String.format(
+                    String.format(
+                        getString(R.string.txt_error_post_notification),
+                        getString(R.string.app_name)
+                    )
+                ),
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction(getString(R.string.goto_settings)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    val settingsIntent: Intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                    startActivity(settingsIntent)
+                }
+            }.show()
+        }
     }
 }
